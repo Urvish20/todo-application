3c588fc487f4456cf9b19bab0e31d4c80492f5f8
Urvish20 <urvishpatel1520@gmail.com>
2025-10-15 09:10:30 +0530

    Enhance Vite configuration by adding alias for source directory

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,17 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
   resolve: {
-    extensions: ['.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@': '/src'
+    }
   }
 })
